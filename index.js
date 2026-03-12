a9040454ccd2473e99bef54dbc5eeb04eee0cd06
toshinkako <toshin.kakou@gmail.com>
2026-03-12 16:28:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -78,9 +78,8 @@ await Promise.all([
 console.log("ログイン成功");
 
 /* 現在時刻 */
-
-const now = new Date();
-
+const now = new Date().toLocaleString("ja-JP", {timeZone: "Asia/Tokyo"});
+ 
 const yyyy = now.getFullYear();
 const mm = String(now.getMonth()+1).padStart(2,"0");
 const dd = String(now.getDate()).padStart(2,"0");
@@ -281,15 +280,7 @@ async function writeSheet(date,time,store,count,staff,total){
 
   requestBody:{
    values:[
-    [
-     date,
-     time,
-     store,
-     count,
-     staff,
-     "",
-     total
-    ]
+    [date,time,store,count,staff.join(","),"",total]
    ]
   }
 
